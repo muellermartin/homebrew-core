7220643ab4cfdfd322b391f76cc4bfab2b95941b
Martin Müller <mail@mueller-martin.net>
2017-03-27 01:46:09 +0200

    liquid-dsp 1.3.0

--- Formula/liquid-dsp.rb
+++ Formula/liquid-dsp.rb
@@ -1,27 +1,17 @@
 class LiquidDsp < Formula
   desc "Signal processing (DSP) library for software-defined radios"
   homepage "http://liquidsdr.org/"
-  url "https://github.com/jgaeddert/liquid-dsp/archive/v1.2.0.tar.gz"
-  sha256 "783854b63f5e9a9830dcd95b2c1d5c0bef7d10d22a0e27844a354b59283b6de3"
+  url "https://github.com/jgaeddert/liquid-dsp/archive/v1.3.0.tar.gz"
+  sha256 "b136343d644bc1441f7854f2d292bfa054e8d040c0b745879b205f6836dca0f0"
   head "https://github.com/jgaeddert/liquid-dsp.git"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "fftw" => :recommended
 
-  # Fix configure checks to be able to compile with LLVM/Clang
-  patch do
-    url "https://github.com/jgaeddert/liquid-dsp/pull/51.patch"
-    sha256 "54dc680ba12c7ed89f667e99b84669770c8b56100df47e4b9ba2f793401c4a96"
-  end
-
   def install
-    system "./reconf"
+    system "./bootstrap.sh"
     system "./configure", "--prefix=#{prefix}"
-    # Note: "make install" in one step does fail
-    # This is reported upstream:
-    # https://github.com/jgaeddert/liquid-dsp/issues/52
-    system "make"
     system "make", "install"
   end
 
